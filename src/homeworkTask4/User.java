d19de1443fbba3dc95c171ffd5d94ce55db66a2f
urales79 <urales@meta.ua>
2016-09-07 00:02:16 +0300

    Home3_2

--- src/homeworkTask4/User.java
+++ src/homeworkTask4/User.java
@@ -1,15 +1,18 @@
 package homeworkTask4;
 
+import java.time.Month;
+
 /**
  * Created by Юрий on 03.09.2016.
  */
 public class User {
     private String name;
     private int balance;
-    private int startOfEmploiment;
+    private int monthsOfEmployment;
     private String CompanyName;
     private int salary;
     private String currency;
+
 
     public void setName(String name) {
         this.name = name;
@@ -19,8 +22,8 @@ public class User {
         this.balance = balance;
     }
 
-    public void setStartOfEmploiment(int startOfEmploiment) {
-        this.startOfEmploiment = startOfEmploiment;
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
     }
 
     public void setCompanyName(String companyName) {
@@ -43,8 +46,9 @@ public class User {
         return balance;
     }
 
-    public int getStartOfEmploiment() {
-        return startOfEmploiment;
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
     }
 
     public String getCompanyName() {
@@ -59,22 +63,41 @@ public class User {
         return currency;
     }
 
-    public User(String name, int balance, int startOfEmploiment, String companyName, int salary, String currency) {
+    public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency) {
         this.name = name;
         this.balance = balance;
-        this.startOfEmploiment = startOfEmploiment;
+        this.monthsOfEmployment = monthsOfEmployment;
         CompanyName = companyName;
         this.salary = salary;
         this.currency = currency;
     }
 
     void paySalary(){
-        setSalary(salary);
+        balance+=salary;
 
     }
-    //Короче я дальше ни фига не понимаю
+   void withdraw(int summ){
+       double comision = balance*0.05;
+       double comision2= balance*0.1;
+
+       if (summ<1000){
+           balance -= comision;
+       }else {
+           balance-=comision2;
+       }
 
 
     }
+    void companyNameLength() {
+
+        System.out.println(CompanyName.length());
+
+
+    }
+   void monthIncreaser(int addMonth){
+       int month = addMonth + monthsOfEmployment;
+   }
+
+}
 
 
